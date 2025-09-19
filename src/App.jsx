75601734cdc96005eb1237010e7344c95166d8a8
saivinay915 <saivinay915@gmail.com>
2025-09-19 20:46:08 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import { EmployeeComponent } from "./components/EmployeeComponent";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
 
